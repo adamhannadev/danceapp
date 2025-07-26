2b717fa5b266b0dfb419b69b209106b932982873
adamhannadev <adamhannadev@gmail.com>
2025-07-26 10:15:48 -0700

    Fix kaminari pagination issue

--- app/controllers/dance_classes_controller.rb
+++ app/controllers/dance_classes_controller.rb
@@ -1,22 +1,93 @@
 class DanceClassesController < ApplicationController
+  before_action :set_dance_class, only: [:show, :edit, :update, :destroy]
+
   def index
+    @dance_classes = DanceClass.includes(:dance_style, :dance_level, :instructor, :location)
+                               .order(:name)
+    
+    # Add pagination if Kaminari is available
+    if defined?(Kaminari)
+      @dance_classes = @dance_classes.page(params[:page]).per(12)
+    else
+      @dance_classes = @dance_classes.limit(50) # Fallback limit
+    end
+    
+    # Filter by dance style if provided
+    if params[:dance_style_id].present?
+      @dance_classes = @dance_classes.where(dance_style_id: params[:dance_style_id])
+    end
+    
+    # Filter by instructor if provided
+    if params[:instructor_id].present?
+      @dance_classes = @dance_classes.where(instructor_id: params[:instructor_id])
+    end
+    
+    @dance_styles = DanceStyle.all
+    @instructors = User.instructors
   end
 
   def show
+    @class_schedules = @dance_class.class_schedules.includes(:bookings)
+    @upcoming_schedules = @class_schedules.where('start_datetime > ?', Time.current)
+                                         .order(:start_datetime)
+                                         .limit(5)
   end
 
   def new
+    @dance_class = DanceClass.new
+    @dance_styles = DanceStyle.all
+    @dance_levels = DanceLevel.all
+    @instructors = User.instructors
+    @locations = Location.active
   end
 
   def create
+    @dance_class = DanceClass.new(dance_class_params)
+    
+    if @dance_class.save
+      redirect_to @dance_class, notice: 'Dance class was successfully created.'
+    else
+      @dance_styles = DanceStyle.all
+      @dance_levels = DanceLevel.all
+      @instructors = User.instructors
+      @locations = Location.active
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @dance_styles = DanceStyle.all
+    @dance_levels = DanceLevel.all
+    @instructors = User.instructors
+    @locations = Location.active
   end
 
   def update
+    if @dance_class.update(dance_class_params)
+      redirect_to @dance_class, notice: 'Dance class was successfully updated.'
+    else
+      @dance_styles = DanceStyle.all
+      @dance_levels = DanceLevel.all
+      @instructors = User.instructors
+      @locations = Location.active
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @dance_class.destroy
+    redirect_to dance_classes_url, notice: 'Dance class was successfully deleted.'
+  end
+
+  private
+
+  def set_dance_class
+    @dance_class = DanceClass.find(params[:id])
+  end
+
+  def dance_class_params
+    params.require(:dance_class).permit(:name, :dance_style_id, :dance_level_id, 
+                                      :instructor_id, :location_id, :duration_minutes, 
+                                      :max_capacity, :price, :description, :class_type)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,63 @@
 module ApplicationHelper
+  def time_duration_in_words(start_time, end_time)
+    return "" unless start_time && end_time
+    
+    duration_minutes = ((end_time - start_time) / 1.minute).round
+    hours = duration_minutes / 60
+    minutes = duration_minutes % 60
+    
+    if hours > 0 && minutes > 0
+      "#{hours}h #{minutes}m"
+    elsif hours > 0
+      "#{hours}h"
+    else
+      "#{minutes}m"
+    end
+  end
+
+  def dance_level_badge_class(level_name)
+    case level_name.downcase
+    when /bronze/
+      "bronze"
+    when /silver/
+      "silver"
+    when /gold/
+      "gold"
+    else
+      ""
+    end
+  end
+
+  def dance_style_category_class(category)
+    case category.downcase
+    when "american smooth"
+      "american_smooth"
+    when "american rhythm"
+      "american_rhythm"
+    when "social"
+      "social"
+    else
+      category.downcase.gsub(' ', '_')
+    end
+  end
+
+  def flash_icon(type)
+    case type.to_s
+    when 'notice', 'success'
+      'fas fa-check-circle'
+    when 'alert', 'error', 'danger'
+      'fas fa-exclamation-triangle'
+    when 'warning'
+      'fas fa-exclamation-circle'
+    else
+      'fas fa-info-circle'
+    end
+  end
+
+  def page_title(title = nil)
+    if title
+      content_for(:title, title)
+    end
+    content_for?(:title) ? content_for(:title) : "Ballroom Dancing CRM"
+  end
 end
