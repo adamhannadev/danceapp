854f39442105c074e80ef133c284ae2483468a05
adamhannadev <adamhannadev@gmail.com>
2025-07-28 21:14:44 -0700

    Update dashboard for instructors

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class DashboardController < ApplicationController
   def index
+    if current_user.instructor?
+      @students = User.joins(:private_lessons_as_student)
+                      .where(private_lessons: { instructor_id: current_user.id })
+                      .distinct.order(:first_name, :last_name)
+      @private_lessons = current_user.private_lessons_as_instructor.order(scheduled_at: :desc).limit(10)
+      @dance_classes = current_user.dance_classes.order(:start_datetime).limit(10)
+    end
   end
 end
