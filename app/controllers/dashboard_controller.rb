ef792b09f866d63579ad4c75080d7bdcd5f75a2a
adamhannadev <adamhannadev@gmail.com>
2025-08-01 21:38:51 -0700

    Set admin content to view if instructor

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,10 +6,15 @@ class DashboardController < ApplicationController
                       .distinct.order(:first_name, :last_name)
       @private_lessons = current_user.private_lessons_as_instructor.order(scheduled_at: :desc).limit(10)
       @dance_classes = current_user.dance_classes.order(:start_datetime).limit(10)
-    else
-      @students = User.where(role: "student")
+      @teachers = User.instructors
+    else if current_user.admin?
+      @students = User.students
       @private_lessons = PrivateLesson.all
       @dance_classes = DanceClass.all
+      @teachers = User.instructors
+    else
+      
     end
+  end
   end
 end
