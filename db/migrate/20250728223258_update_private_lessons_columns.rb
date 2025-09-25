4c74b2318480e124af4374a9e85825482992c626
adamhannadev <adamhannadev@gmail.com>
2025-09-25 15:24:30 -0700

    Fix typo in UpdatePrivateLessonsColumns migration

--- db/migrate/20250728223258_update_private_lessons_columns.rb
+++ db/migrate/20250728223258_update_private_lessons_columns.rb
@@ -5,6 +5,6 @@ class UpdatePrivateLessonsColumns < ActiveRecord::Migration[7.2]
     change_column_default :private_lessons, :status, 'requested'
     
     # Add precision to cost column
-    change_column :private_lessons, :cost, :decimal, precision: 8, scale: 2
+    add_column :private_lessons, :cost, :decimal, precision: 8, scale: 2
   end
 end
